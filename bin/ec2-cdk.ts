691662828e0dadda1621de5e14b8ec179c2dc213
natalie-elizabeth <natalie.elizabeth@andela.com>
2019-08-05 22:55:40 +0300

    Initial commit

---
+++ bin/ec2-cdk.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { Ec2CdkStack } from '../lib/ec2-cdk-stack';
+
+const app = new cdk.App();
+new Ec2CdkStack(app, 'Ec2CdkStack');
