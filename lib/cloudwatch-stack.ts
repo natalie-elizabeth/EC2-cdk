694b3947634db0d1c114ea3ec5ba5630a29e78f0
natalie-elizabeth <natalie.elizabeth@andela.com>
2019-08-08 16:00:42 +0300

    update'
    Signed-off-by: natalie-elizabeth <natalie.elizabeth@andela.com>

--- lib/cloudwatch-stack.ts
+++ lib/cloudwatch-stack.ts
@@ -2,13 +2,24 @@ import cdk = require('@aws-cdk/core');
 import cloudwatch = require('@aws-cdk/aws-cloudwatch');
 import { GraphWidget, Metric } from '@aws-cdk/aws-cloudwatch';
 
-export class CdkAppStack extends cdk.Stack {
+export class CloudwatchAppStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const sfdashboard = new cloudwatch.Dashboard(this, 'SfDashboard', { 
+        const metric = new Metric({
+            namespace: 'AWS/SQS',
+            metricName: 'ApproximateNumberOfMessagesVisible',
+        });
+
+        const sfdashboard = new cloudwatch.Dashboard(this, 'SfDashboard', {
             dashboardName: 'SfDashboard'
         });
-        
+
+        sfdashboard.addWidgets(new GraphWidget({
+            title: "Experiment one",
+            left: [metric],
+            right: [metric]
+
+        }));
     }
 }
